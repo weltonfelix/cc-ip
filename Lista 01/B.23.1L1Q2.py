5d5b9c11bb8dddad17f2145b319e01e439507dcb
Welton Felix <hi@weltonfelix.dev>
2023-06-12 11:10:07 -0300

    Lista 01 - B

---
+++ Lista 01/B.23.1L1Q2.py
@@ -0,0 +1,18 @@
+local = input()
+case = input()
+
+if local == "Queens" or local == "Times square" or local == "Central Park":
+    print("Parece que hoje teremos que dar umas teiada por aí... simboora!")
+
+    if local == "Queens":
+        print(f"Eita... {case}, e logo aqui perto da tia May viiu?!")
+    elif local == "Times square":
+        print(
+            f"Movimentação intensa de cria aqui pra variar, mas tem {case}, não posso deixar isso acontecer!"
+        )
+    elif local == "Central Park":
+        print(f"Nossa... {case}, depois de salvar vou pegar um hot dog meermo.")
+else:
+    print(
+        "Tudo maravilindo! mas tenho que estudar para prova de Inglês e História ainda boy..."
+    )
